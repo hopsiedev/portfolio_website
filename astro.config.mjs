806bd1619e0b02b357f2bb5f5a1c0d3523619f9e
Danilson Santana <officialdanny11@gmail.com>
2024-11-07 21:13:59 +0000

    vercel app

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,3 +5,11 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [tailwind(), react()],
 });
+
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+});
