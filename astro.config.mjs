67758df25550022a5784554f156daa453d3ee69b
Danilson Santana <officialdanny11@gmail.com>
2024-11-07 21:17:40 +0000

    remove vercel app

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,3 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [tailwind(), react()],
 });
-
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-
-export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-});
